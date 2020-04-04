c8a0186528daf023509db8319726ee6846700b17
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-04 10:38:59 -0400

    added vowel checker functionality to longwords - Exercise Complete

--- longwords.py
+++ longwords.py
@@ -1,7 +1,8 @@
 # given a word as a string, extend any long vowels to the length of five
-# get user input and set up compare variable
+# get user input and set up compare variable and vowels variable
 inp = input("Give me a string! ")
 compare = ''
+vowels = 'aeiouy'
 # create a new variable to return to the new string, will convert list to string
 new_string = []
 # check each letter in the input
@@ -10,9 +11,11 @@ for index in range(len(inp)):
     letter = inp[index]
     # test and see if previous letter = current letter
     if compare == letter:
-        # if previous letter equals current letter, multiply letter by 4
-        letter = letter * 4
-        new_string.append(letter)
+        # test if double letters are vowels
+        if letter in vowels:
+            # if previous letter equals current letter and is a vowel, multiply letter by 4
+            letter = letter * 4
+            new_string.append(letter)
     # if the letters don't match, add only one letter
     else:
         new_string.append(letter)
