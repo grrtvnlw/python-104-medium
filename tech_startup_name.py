ed25b5d73407ad08e5c6f5576c59e3ab0f68b220
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-05 15:42:33 -0400

    Completed Tech Startup Name exercise

--- tech_startup_name.py
+++ tech_startup_name.py
@@ -1,10 +1,10 @@
-# given a name from a user, remove the last vowel from the name
+# given a name from a user, remove the last vowel that comes before a non-vowel
 inp = input("What's the name of your startup? ")
 # create vowels variable and current char, next char holders
 vowels = 'aeiouy'
 next_char = ''
 current_char = ''
-# creat a variable for the new string that is a list
+# creat a variable for the new string that will be a list for now
 new_string = []
 # create counter variable and set equal to one less than len(input) so we can start at the last index
 counter = (len(inp) - 1)
@@ -19,14 +19,14 @@ while 0 <= counter:
     # if current letter is not a vowel, add it to the front of the new string
     if current_char not in vowels:
        new_string.insert(0, current_char) 
-    # if current character is a vowel, check the conditions
+    # if current character is a vowel, check the following conditions
     elif current_char in vowels:
         # if it is a vowel and the last letter of the string, add it to the new string
         if current_char == inp[-1]:
             new_string.insert(0, current_char)
         # if it is a vowel and not the last letter
         elif current_char in vowels:
-            # do not add the vowel / close the on-off switch
+            # do not add the vowel / skip this vowel and close the on-off switch
             if skip == 0:
                 skip = 1
             # the next vowel will be added to the new string since the skip switch is now off
