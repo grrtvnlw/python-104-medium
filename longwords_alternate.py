7fcd73872a7d75b22a69d97e3d4d0b6655dcab68
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-05 16:06:54 -0400

    Refactored code: renamed variables and updated comments

--- longwords_alternate.py
+++ longwords_alternate.py
@@ -1,25 +1,27 @@
 # given a word as a string, extend any long vowels to the length of five
 # get user input and set up compare variable and vowels variable
 inp = input("Give me a string! ")
-compare = ''
+prev_char = ''
 vowels = 'aeiouy'
-# create a variable for the new string and set it equal to an empty string
+# create a variable for the new string
 new_string = ''
 # check each letter in the input
 for index in range(len(inp)):
-    # letter equals the value at each index of the input
-    letter = inp[index]
+    # current letter equals the current index value
+    current_char = inp[index]
     # test and see if previous letter = current letter
-    if compare == letter:
+    if prev_char == current_char:
         # test if double letters are vowels
-        if letter in vowels:
+        if current_char in vowels:
             # if previous letter equals current letter and is a vowel, multiply letter by 4
-            letter = letter * 4
-            new_string += letter
-    # if the letters don't match, add only one letter
+            current_char *= 4
+            new_string += current_char
+        else: 
+            new_string += current_char
+    # if the letters don't match, add only the current letter
     else:
-        new_string += letter
-    # set compare variable to current letter and end of loop
-    compare = inp[index]
+        new_string += current_char
+    # set previous letter to current letter at end of loop
+    prev_char = inp[index]
 # print the new string
 print(new_string)
